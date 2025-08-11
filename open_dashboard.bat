8d2e13ab14fc2f91496aa85ba12f8f6ea1e3c192
hwangchahae <wndal12@gmail.com>
2025-08-11 16:33:55 +0900

    정리commit

--- open_dashboard.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Opening dashboard in browser...
-timeout /t 2 /nobreak > nul
-start http://localhost:5000
-echo Dashboard opened in browser!
-echo If page doesn't load, make sure the server is running (run start_dashboard.bat first)
